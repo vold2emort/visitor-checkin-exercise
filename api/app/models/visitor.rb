a4a039f4c392dccbd6c6495560cdf93938f648f1
vold2emort <pramishkc066@gmail.com>
2026-09-14 12:58:39 +0545

    fix: Validate required fields and set timezone to Asia/Kathmandu

--- api/app/models/visitor.rb
+++ api/app/models/visitor.rb
@@ -1,3 +1,8 @@
 class Visitor < ApplicationRecord
   belongs_to :host, optional: true
+
+  validates :full_name, presence: true
+  validates :company_name, presence: true
+  validates :purpose, presence: true
+  validates :host, presence: true
 end
--- api/config/application.rb
+++ api/config/application.rb
@@ -34,6 +34,7 @@ module Api
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Kathmandu"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
